fdab1b57e6510ffb29eb97a9bead1bd8319a802a
PavanKumarV741 <102721175+PavanKumarV741@users.noreply.github.com>
2023-02-24 16:10:16 +0530

    added responsive with onhover effect.

--- src/App.js
+++ src/App.js
@@ -2,74 +2,73 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [result,setresult]=useState("")
-  
-  const handleclick=(e)=>{
+  const [result, setresult] = useState("")
+
+  const handleclick = (e) => {
     setresult(result.concat(e.target.name))
   }
-  const clear=()=>{
+  const clear = () => {
     setresult("")
   }
-  
-  const backspace=()=>{
-    setresult(result.slice(0,-1))
+
+  const backspace = () => {
+    setresult(result.slice(0, -1))
   }
 
-  const calculate= ()=>{
-    try{
+  const calculate = () => {
+    try {
       setresult(eval(result))
     }
-    catch(err){
+    catch (err) {
       console.log(err)
     }
   }
 
   return (
-        <div className='cal'>
-          <h2>Calculator</h2>
-            <form className='inputfield'>
-            <input type="text" value={result} placeholder="=" />
-            </form>
-            <br/>
-            <div className="keypad">
+    <div className='cal' >
+      <h2>Calculator</h2>
+      <form className='inputfield'>
+        <input type="text" value={result} placeholder="=" />
+      </form>
+      <br />
+      <div className="keypad">
 
-            <div className='clear_backspace'>
+          <div className='clear_backspace'>
             <button className='highlighted' onClick={clear}>Clear</button>
-            <button  className='highlighted' onClick={backspace}>Delete</button>
-            <div/>
-
+            <button className='highlighted' onClick={backspace}>Delete</button>
+          <div/>
+          <div className='keypadbuttons'>
             <div>
-            <button className='highlighted' name='+' onClick={handleclick}>+</button>
-            <button className='highlighted' name='-' onClick={handleclick}>-</button>
-            <button className='highlighted' name='/' onClick={handleclick}>/</button>
-            <button className='highlighted' name='*' onClick={handleclick}>*</button>
+              <button className='highlighted' name='+' onClick={handleclick}>+</button>
+              <button className='highlighted' name='-' onClick={handleclick}>-</button>
+              <button className='highlighted' name='/' onClick={handleclick}>/</button>
+              <button className='highlighted' name='*' onClick={handleclick}>*</button>
             </div>
 
             <div>
-            <button className='highlighted' name='.' onClick={handleclick}>.</button>
-            <button name='0' onClick={handleclick}>0</button>
-            <button name='1' onClick={handleclick}>1</button>
-            <button name='2' onClick={handleclick}>2</button>
+              <button className='highlighted' name='.' onClick={handleclick}>.</button>
+              <button name='0' onClick={handleclick}>0</button>
+              <button name='1' onClick={handleclick}>1</button>
+              <button name='2' onClick={handleclick}>2</button>
             </div>
 
             <div>
-            <button name='3' onClick={handleclick}>3</button>
-            <button name='4' onClick={handleclick}>4</button> 
-            <button name='5' onClick={handleclick}>5</button>
-            <button name='6' onClick={handleclick}>6</button>
+              <button name='3' onClick={handleclick}>3</button>
+              <button name='4' onClick={handleclick}>4</button>
+              <button name='5' onClick={handleclick}>5</button>
+              <button name='6' onClick={handleclick}>6</button>
             </div>
 
             <div>
-            <button name='7' onClick={handleclick}>7</button>
-            <button name='8' onClick={handleclick}>8</button>
-            <button name='9' onClick={handleclick}>9</button>
-            <button className='highlighted' name='=' onClick={calculate}>=</button>
+              <button name='7' onClick={handleclick}>7</button>
+              <button name='8' onClick={handleclick}>8</button>
+              <button name='9' onClick={handleclick}>9</button>
+              <button className='highlighted' name='=' onClick={calculate}>=</button>
             </div>
-
-
+          </div>
         </div>
-        </div>
-       </div> 
+      </div>
+    </div>
   );
 }
 
